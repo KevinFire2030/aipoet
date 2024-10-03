790d80867a9353d087923533c56b9560ccd1d2b5
KevinFire2030 <109524169+KevinFire2030@users.noreply.github.com>
2024-10-04 02:31:08 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
+import streamlit as st
+from langchain_openai import OpenAI, ChatOpenAI
+
+# 환경 변수에서 API 키 가져오기
+api_key = os.getenv("OPENAI_API_KEY")
+
+
+
+# ChatOpenAI 테스트
+chat_model = ChatOpenAI(api_key=api_key)
+
+st.title("인공지능 시인")
+
+content = st.text_input("시의 주재를 재시해주세요")
+
+if st.button("시 작성 요청하기"):
+
+    with st.spinner('시 작성 중...'):
+        result = chat_model.invoke(content + "에 대한 삼행시를 써줘")
+        st.write(result.content)
+
+
+    
+
+
+
+
+
+
